ce47a8c8fe06e2084852ed999e74bd40a4433b4b
gusadolfo123 <gusadolfo123@hotmail.com>
2019-10-11 21:09:26 -0500

    Clase 11

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ import AutoPlay from './plugins/AutoPlay.js';
 const video = document.querySelector('video');
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+const buttonPlay = document.querySelector('.button-play');
+buttonPlay.onclick = () => player.togglePlay();
+
+const buttonSound = document.querySelector('.button-sound');
+buttonSound.onclick = () => player.toggleSound();
--- mediaPlayer.js
+++ mediaPlayer.js
@@ -20,11 +20,21 @@ MediaPlayer.prototype.pause = function() {
 };
 
 MediaPlayer.prototype.togglePlay = function() {
-	if (this.media.paused) {
-		this.play();
-	} else {
-		this.pause();
-	}
+	if (this.media.paused) this.play();
+	else this.pause();
+};
+
+MediaPlayer.prototype.mute = function() {
+	this.media.muted = true;
+};
+
+MediaPlayer.prototype.unmute = function() {
+	this.media.muted = false;
+};
+
+MediaPlayer.prototype.toggleSound = function() {
+	if (this.media.muted) this.media.muted = false;
+	else this.media.muted = true;
 };
 
 export default MediaPlayer;
