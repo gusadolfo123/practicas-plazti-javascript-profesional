c0878c52e6da0e0d5295decec5fa71e7f761140c
gusadolfo123 <gusadolfo123@hotmail.com>
2019-10-13 21:50:01 -0500

    Getters and setters

--- mediaPlayer.js
+++ mediaPlayer.js
@@ -5,9 +5,23 @@ function MediaPlayer(config) {
 	this._initPlugins();
 }
 
+// se puede usar los get and set para no pasar todo el this,
+// sino solo una parte
+
 MediaPlayer.prototype._initPlugins = function() {
+	const player = {
+		play: () => this.play(),
+		pause: () => this.pause(),
+		media: this.media,
+		get muted() {
+			return this.media.muted;
+		},
+		set muted(value) {
+			this.media.muted = value;
+		},
+	};
 	this.plugins.forEach(plugin => {
-		plugin.run(this);
+		plugin.run(player);
 	});
 };
 
